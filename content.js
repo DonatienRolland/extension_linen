19067a4cab8cd987e2d4e4a5e5f9520e11b40e0c
victor ledoda <lebodavictor@gmail.com>
2017-12-06 18:43:17 +0100

    the scraper

--- content.js
+++ content.js
@@ -1,19 +1,16 @@
-console.log("my formated data:");
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+  console.log("DOM fully loaded and parsed");
 
+  console.log("my formated data:");
 
-const brand_name = document.querySelector('.product-description a:nth-of-type(2) strong').innerText
-const product_code = document.querySelector('.product-code span').innerText
-const category = document.querySelector('.product-description a:nth-of-type(1) strong').innerText
-const percent = document.querySelector('.about-me').innerText.split(" : ")[1].split("%")[0]
-const material = document.querySelector('.about-me').innerText.split("%")[1].split(".")[0]
-const price = document.querySelector('.current-price').innerText.split(" €")[0]
-const image = document.querySelectorAll(".fullImageContainer img")[1].src
-  // const image = document.querySelector('div.fullImageContainer img').src
-const title = document.querySelector('.product-hero > h1').innerText.split(" - ")[1]
-
-  // const url = 'https://www.linenapp.eu/items'
-  // const formatedUrl = url + '?title=' + title.replace(/ /g,"-") + '?product_code=' + product_code.replace(/ /g,"-") + '?category=' + category.replace(/ /g,"-") + '?brand=' + brand_name.replace(/ /g,"-") + '?material_name=' + material_name.replace(/./g, "" ) + ?assembling=' + assembling + '?price=' + price + '?image=' + image
-  // return formatedUrl
+  const brand_name = document.querySelector('.product-description a:nth-of-type(2) strong').innerText
+  const product_code = document.querySelector('.product-code span').innerText
+  const category = document.querySelector('.product-description a:nth-of-type(1) strong').innerText
+  const percent = document.querySelector('.about-me').innerText.split(" : ")[1].split("%")[0]
+  const name = document.querySelector('.about-me').innerText.split("%")[1].split(".")[0].replace(/^\s+/g, '')
+  const price = document.querySelector('.current-price').innerText.split(" €")[0]
+  const image = document.querySelectorAll('.fullImageContainer img')[1].src
+  const title = document.querySelector('.product-hero > h1').innerText.split(" - ")[1]
 
   const formatedData = {
     brandName: brand_name,
@@ -22,12 +19,17 @@ const title = document.querySelector('.product-hero > h1').innerText.split(" - "
     assemblings: [
     {
       percent: percent,
-      material: { name: name, }
+      material: name,
     }],
     price: price,
     image: image,
     title: title
   }
 
-    console.log(JSON.stringify(formatedData))
-// How to generate file.json ?
+  console.log(JSON.stringify(formatedData));
+  sendResponse({data: JSON.stringify(formatedData)})
+
+});
+// const url = 'https://www.linenapp.eu/items'
+// const formatedUrl = url + '?title=' + title.replace(/ /g,"-") + '?product_code=' + product_code.replace(/ /g,"-") + '?category=' + category.replace(/ /g,"-") + '?brand=' + brand_name.replace(/ /g,"-") + '?material_name=' + material_name.replace(/./g, "" ) + ?assembling=' + assembling + '?price=' + price + '?image=' + image
+// return formatedUrl
--- popup.js
+++ popup.js
@@ -1,10 +1,37 @@
 
-function getLinenApp(){
-  const body = document.querySelector('body');
-  fetch("https://")
-    .then(response => response.text());
-    .then((html) => {
-      const item = element.querySelector('');
-      body.insertAdjacentHTML("beforeend", item);
+// function getLinenApp(){
+//   const body = document.querySelector('body');
+//   fetch("https://")
+//   .then(response => response.text());
+//   .then((html) => {
+//     const item = element.querySelector('');
+//     body.insertAdjacentHTML("beforeend", item);
+//   });
+// };
+
+document.addEventListener('DOMContentLoaded', function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {data: 'test'}, function(response) {
+      if (response) {
+        const dataToSend = response.data;
+        fetch("http://localhost:3000/api/v1/items",
+        {
+          method: "POST",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(dataToSend)
+        })
+        .then(function(res){
+          console.log(res.statusText);
+        })
+        .catch(function(data){
+          console.error( data )
+        });
+      } else {
+        console.log("Not there, inject contentscript");
+      }
     });
-};
+  });
+});
