de704e553bef7a3faf744f5ea9315c1699880d65
victor ledoda <lebodavictor@gmail.com>
2017-12-07 12:01:08 +0100

    added beautiful chrome extension icon

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {data: 'test'}, function(response) {
